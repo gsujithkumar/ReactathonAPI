56ad869987b9ac4f84f2dda3b75bd5ab8f1294e5
Suman <sumananandan@hotmail.com>
2018-07-03 13:56:43 +0000

    added cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const routes = require('./server/routes/index');
 // var users = require('./routes/users');
@@ -16,6 +17,8 @@ const app = express();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'client')));
 
 app.use('/', routes);
 // app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
