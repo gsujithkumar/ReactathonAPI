e91da51fe865b3fc2e9fe59d88095e11b2d0e0cb
Sujith <sujithkumar.g24@gmail.com>
2018-07-03 10:47:06 +0000

    changed app.js

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const routes = require('./server/routes/index');
 const app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'html');
+ app.set('views', path.join(__dirname, 'views'));
+ app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
